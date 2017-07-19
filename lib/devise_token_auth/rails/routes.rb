15e4eed8215996fb5bd809ed9691211b8729531a
ruvzi <zarevo@ukr.net>
2017-07-19 13:30:45 +0300

    omniauth_callback_path

--- lib/devise_token_auth/rails/routes.rb
+++ lib/devise_token_auth/rails/routes.rb
@@ -57,7 +57,7 @@ module ActionDispatch::Routing
             # omniauth routes. only define if omniauth is installed and not skipped.
             if defined?(::OmniAuth) and not opts[:skip].include?(:omniauth_callbacks)
               match "#{full_path}/failure",             controller: omniauth_ctrl, action: 'omniauth_failure', via: [:get]
-              match "#{full_path}/:provider/callback",  controller: omniauth_ctrl, action: 'omniauth_success', via: [:get]
+              match "#{full_path}/:provider/callback",  controller: omniauth_ctrl, action: 'omniauth_success', via: [:get], as: :omniauth_callback
 
               match "#{DeviseTokenAuth.omniauth_prefix}/:provider/callback", controller: omniauth_ctrl, action: 'redirect_callbacks', via: [:get]
               match "#{DeviseTokenAuth.omniauth_prefix}/failure", controller: omniauth_ctrl, action: 'omniauth_failure', via: [:get]
