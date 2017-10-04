0f9722067d6d88fb990b0a6025d0d222f06eb8e9
ruvzi <zarevo@ukr.net>
2017-10-04 12:48:34 +0300

    add current_city_names to users

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.sanitize(:sign_up) << :operating_thetan
     devise_parameter_sanitizer.sanitize(:sign_up) << :favorite_color
     devise_parameter_sanitizer.sanitize(:account_update) << :current_password
-    devise_parameter_sanitizer.sanitize(:account_update) += SETTS['account_update_params'] || []
   end
 end
